ad54a270f482874cf7baf7717fd941ddca9c5db7
Raj0705 <120808590+Raj0705@users.noreply.github.com>
2023-02-21 12:29:14 +0530

    Add files via upload

---
+++ Quizzing App/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+// Only have two classes for this
+public class Main {
+    public static void main(String[] args) {
+
+        QuizzingApps quiz = new QuizzingApps() ;
+    }
+
+}
